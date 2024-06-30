9f9e4f9b3c65e4b3908889f00754104e12160314
Prashant Lodhi <prashantrajput2517@gmail.com>
2024-06-30 16:40:53 +0530

    changes for Youtube API calling

--- youtubeAPI/app.js
+++ youtubeAPI/app.js
@@ -1,43 +1,16 @@
+import URL from "../.env"
 const videoSection = document.querySelector("section");
 const loader = document.querySelector('.container')
 
 
-// document.getElementById('recipe-form').addEventListener("submit", function(event){
-//     event.preventDefault();
 
-//     const text = document.getElementById('inputvalue').value;
-    
-//     const URL = `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyApD-zTzkXsCUmAv1QfBbLtqdn-0YJg6Sw&q=${text}&type=video&part=snippet`;
-    
-//     loader.style.display = "block";
-    
-//     setTimeout(getVideos, 3000);
-// })
-// function getVideos(){
-    
-//     fetch(URL)
-//         .then(res => res.json())
-//             .then(data => {
-//                 loader.style.display = "none";
-//                 data.items.forEach(element => {
-//                     videoSection.innerHTML += `
-//                     <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}" class="yt-video"/>
-//                         <img src="${element.snippet.thumbnails.maxres.url}" />
-//                         <h3>${element.snippet.title}</h3>
-//                         </a>` 
-//                 });
-//     }).catch(err => {
-//         loader.style.display = "none";
-//         videoSection.innerHTML = '<h3>Sorry! Something went wrong</h3>'
-//     })
-// }
-
+let api;
 document.getElementById('recipe-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from submitting
 
     const text = document.getElementById('inputvalue').value;
 
-    const URL = `https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyApD-zTzkXsCUmAv1QfBbLtqdn-0YJg6Sw&q=${text}&type=video&part=snippet`;
+    api = `${URL}}?q=${encodeURIComponent(text)}`
 
     loader.style.display = "block"; // Show loader before fetching data
 
@@ -45,7 +18,7 @@ document.getElementById('recipe-form').addEventListener('submit', function(event
 });
 
 function getVideos(){
-    fetch(URL)
+    fetch(api)
         .then(res => res.json())
             .then(data => {
                 loader.style.display = "none";
